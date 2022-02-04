64f5f1e8749ec002f01ab72e58bbbebda6cd0142
Adam Daniels <adam@mediadrive.ca>
2022-02-04 15:31:02 -0500

    Implement router

--- framework-core/lib/framework.rb
+++ framework-core/lib/framework.rb
@@ -1,15 +1,57 @@
+# frozen-string-literal: true
+require "hanami/router"
+
 module Framework
   module Action
-  end
+    def self.included(action)
+      super
 
-  class Application
-    def self.start
-      ->(env) { [200, {}, ["OK"]] }
+      action.class_eval do
+        attr_accessor :request
+        attr_accessor :response
+
+        def self.call(env)
+          new.tap do |obj|
+            obj.request = Rack::Request.new(env)
+            obj.response = Rack::Response.new
+          end.call
+        end
+      end
     end
   end
 
+  class Application
+    attr_reader :router
+
+    def initialize(router)
+      @router = router
+    end
+
+    def to_app
+      @router
+    end
+
+    def self.namespace
+      to_s.chomp("::Application")
+    end
+
+    def self.start
+      routes = Kernel.const_get(namespace)::Routes.routes
+      router = Router.new(&routes)
+      new(router).to_app
+    end
+  end
+
+  class Router < ::Hanami::Router
+  end
+
   class Routes
-    def self.define
+    def self.define(&blk)
+      @routes = blk
+    end
+
+    def self.routes
+      @routes || proc {}
     end
   end
 end
