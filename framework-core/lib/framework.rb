27c1ac4bef545ee231944c92a0bfc3755048bb5c
Adam Daniels <adam@mediadrive.ca>
2022-03-23 19:53:01 -0400

    Evaluate block inside our router instance

--- framework-core/lib/framework.rb
+++ framework-core/lib/framework.rb
@@ -155,7 +155,8 @@ module Framework
 
   class Router
     def initialize(base_url:, resolver:, &blk)
-      @router = Hanami::Router.new(base_url: base_url, resolver: resolver, &blk)
+      @router = Hanami::Router.new(base_url: base_url, resolver: resolver)
+      instance_eval(&blk) if blk
     end
 
     def self.build(application)
@@ -170,6 +171,58 @@ module Framework
       new(base_url: application.config.base_url, resolver: resolver, &route_class.routes)
     end
 
+    def get(path, to: nil, as: nil, **constraints, &blk)
+      @router.get(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def post(path, to: nil, as: nil, **constraints, &blk)
+      @router.post(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def patch(path, to: nil, as: nil, **constraints, &blk)
+      @router.patch(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def put(path, to: nil, as: nil, **constraints, &blk)
+      @router.put(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def delete(path, to: nil, as: nil, **constraints, &blk)
+      @router.delete(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def link(path, to: nil, as: nil, **constraints, &blk)
+      @router.link(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def unlink(path, to: nil, as: nil, **constraints, &blk)
+      @router.unlink(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def options(path, to: nil, as: nil, **constraints, &blk)
+      @router.options(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def trace(path, to: nil, as: nil, **constraints, &blk)
+      @router.trace(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def mount(app, at:, **constraints)
+      @router.mount(app, at: at, **constraints)
+    end
+
+    def redirect(path, to: nil, as: nil, code: DEFAULT_REDIRECT_CODE)
+      @router.redirect(path, to: to, as: as, code: code)
+    end
+
+    def scope(path, &blk)
+      @router.scope(path, &blk)
+    end
+
+    def root(to: nil, &blk)
+      @router.root(to: to, &blk)
+    end
+
     def path(...)
       @router.path(...)
     end
@@ -181,6 +234,8 @@ module Framework
     def call(...)
       @router.call(...)
     end
+
+    DEFAULT_REDIRECT_CODE = 301
   end
 
   class Routes
