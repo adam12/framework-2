1917f636877c6c1e280cd78a5f93550f0d27064a
Adam Daniels <adam@mediadrive.ca>
2022-04-23 20:33:52 -0400

    Start running server in thread instead of process
    Use a RunningServer class to wrap starting and stopping of server. Yield
    a RunningServer instance with a `uri` method for easy HTTP requests.

--- framework-core/test/system/test_app.rb
+++ framework-core/test/system/test_app.rb
@@ -45,16 +45,37 @@ class TestApp < Minitest::Test
         run Blog::Application.start(base_url: "http://example.com")
       RUBY
 
-      child = spawn("rackup -q -p 61333 -I#{Dir.pwd}/lib config.ru", chdir: dir)
-      # Wait for process to boot
-      sleep 2
+      RunningServer.start(dir + "/config.ru") do |running_server|
+        assert_match "OK", Net::HTTP.get(running_server.uri)
+        assert_match "Blog post 4 at http://example.com/posts/4", Net::HTTP.get(running_server.uri + "/posts/4")
+      end
+    end
+  end
 
-      assert_match "OK", Net::HTTP.get(URI("http://localhost:61333"))
-      assert_match "Blog post 4 at http://example.com/posts/4", Net::HTTP.get(URI("http://localhost:61333/posts/4"))
+  RunningServer = Struct.new(:hostname, :port) do
+    def uri
+      URI("http://#{hostname}:#{port}")
+    end
+
+    def self.start(rackup_file)
+      require "rack"
+      app, _options = Rack::Builder.load_file(rackup_file)
+      server = Rack::Server.new(app: app, Port: 61333, AccessLog: [])
+      Thread.new do
+        server.start
+      end
+
+      loop do
+        TCPSocket.new("localhost", 61333)
+        break
+      rescue
+        sleep 0.1
+        retry
+      end
+
+      yield new("localhost", 61333)
     ensure
-      Process.kill("INT", child) if child
-      # Wait for child to exit successfully
-      Process.waitall
+      server&.server&.shutdown
     end
   end
 end
