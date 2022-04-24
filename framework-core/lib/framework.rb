c0f3931fa964bf33b11cf4b2e46858cfd3b9f4d9
Adam Daniels <adam@mediadrive.ca>
2022-04-24 08:35:57 -0400

    [framework-core] Delegate to Hanami Router instead of inheriting
    Squashed commit of the following:
    
    commit 27c1ac4bef545ee231944c92a0bfc3755048bb5c
    Author: Adam Daniels <adam@mediadrive.ca>
    Date:   Wed Mar 23 19:53:01 2022 -0400
    
        Evaluate block inside our router instance
    
    commit a161cd39a2cc129e86f5f1c0dfc597a51f1801dd
    Author: Adam Daniels <adam@mediadrive.ca>
    Date:   Wed Mar 23 18:04:00 2022 -0400
    
        Delegate to Hanami Router instead of inheriting

--- framework-core/lib/framework.rb
+++ framework-core/lib/framework.rb
@@ -153,7 +153,12 @@ module Framework
     end
   end
 
-  class Router < ::Hanami::Router
+  class Router
+    def initialize(base_url:, resolver:, &blk)
+      @router = Hanami::Router.new(base_url: base_url, resolver: resolver)
+      instance_eval(&blk) if blk
+    end
+
     def self.build(application)
       resolver = Framework::Resolver.new(application)
       begin
@@ -165,6 +170,72 @@ module Framework
 
       new(base_url: application.config.base_url, resolver: resolver, &route_class.routes)
     end
+
+    def get(path, to: nil, as: nil, **constraints, &blk)
+      @router.get(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def post(path, to: nil, as: nil, **constraints, &blk)
+      @router.post(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def patch(path, to: nil, as: nil, **constraints, &blk)
+      @router.patch(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def put(path, to: nil, as: nil, **constraints, &blk)
+      @router.put(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def delete(path, to: nil, as: nil, **constraints, &blk)
+      @router.delete(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def link(path, to: nil, as: nil, **constraints, &blk)
+      @router.link(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def unlink(path, to: nil, as: nil, **constraints, &blk)
+      @router.unlink(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def options(path, to: nil, as: nil, **constraints, &blk)
+      @router.options(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def trace(path, to: nil, as: nil, **constraints, &blk)
+      @router.trace(path, to: to, as: as, **constraints, &blk)
+    end
+
+    def mount(app, at:, **constraints)
+      @router.mount(app, at: at, **constraints)
+    end
+
+    def redirect(path, to: nil, as: nil, code: DEFAULT_REDIRECT_CODE)
+      @router.redirect(path, to: to, as: as, code: code)
+    end
+
+    def scope(path, &blk)
+      @router.scope(path, &blk)
+    end
+
+    def root(to: nil, &blk)
+      @router.root(to: to, &blk)
+    end
+
+    def path(...)
+      @router.path(...)
+    end
+
+    def url(...)
+      @router.url(...)
+    end
+
+    def call(...)
+      @router.call(...)
+    end
+
+    DEFAULT_REDIRECT_CODE = 301
   end
 
   class Routes
