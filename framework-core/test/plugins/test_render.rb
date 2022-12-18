d49b8cf1a4d5d0b92da564d5c92196509f172087
Adam Daniels <adam@mediadrive.ca>
2022-12-17 20:19:24 -0500

    Test locals functionality

--- framework-core/test/plugins/test_render.rb
+++ framework-core/test/plugins/test_render.rb
@@ -31,6 +31,20 @@ module Framework
 
         assert_equal "Layout with Render test string\n\n", action.new.call
       end
+
+      def test_render_with_locals
+        application = Class.new(Framework::Application) do
+          plugin Framework::Plugins::Render
+        end
+
+        action = Class.new(application::FrameworkAction) do
+          def call
+            render(__dir__ + "/render_with_locals.str", foobar: "foobar")
+          end
+        end
+
+        assert_equal "Render with locals foobar\n", action.new.call
+      end
     end
   end
 end
