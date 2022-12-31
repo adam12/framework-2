b2da6f67cbe9ebd90356c45f40c4025f6213f2e9
Adam Daniels <adam@mediadrive.ca>
2022-12-31 14:02:27 -0500

    Testing middleware stack

---
+++ framework-core/test/plugins/http_router/test_middleware_stack.rb
@@ -0,0 +1,34 @@
+require "test_init"
+require "framework/plugins/http_router/middleware_stack"
+
+module Framework
+  module Plugins
+    module HttpRouter
+      class TestMiddlewareStack < Minitest::Test
+        def setup
+          super
+          @stack = MiddlewareStack.new
+        end
+
+        def test_use
+          middleware = Object.new
+          @stack.use(middleware)
+          assert_equal [[middleware, []]], @stack.to_a
+        end
+
+        def test_use_with_args
+          middleware = Object.new
+          args = Object.new
+          @stack.use(middleware, args)
+          assert_equal [[middleware, [args]]], @stack.to_a
+        end
+
+        def test_default_stack
+          stack = MiddlewareStack.default
+
+          refute_empty stack.to_a
+        end
+      end
+    end
+  end
+end
