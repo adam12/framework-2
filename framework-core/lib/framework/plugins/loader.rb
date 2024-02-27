0dcb91e86153593631219cf45c3ef73f4335094f
Adam Daniels <adam@mediadrive.ca>
2024-02-27 12:39:10 -0500

    [core] Loader eager-loading in non dev/test

--- framework-core/lib/framework/plugins/loader.rb
+++ framework-core/lib/framework/plugins/loader.rb
@@ -4,16 +4,23 @@ module Framework
       def self.before_load(application, log: false, &block)
         application.plugin Variant
 
+        enable_reloading = application.variant.development?
+        enable_eager_loading = !application.variant.development? && !application.variant.testing?
+
         require "zeitwerk"
 
         loader = Zeitwerk::Loader.new
         loader.log! if log
 
-        loader.enable_reloading if application.variant.development?
+        loader.enable_reloading if enable_reloading
 
         loader.push_dir(File.join(Dir.pwd, "lib"))
+
         block&.call(loader)
+
         loader.setup
+        loader.eager_load if enable_eager_loading
+
         application.const_set(:Loader, loader)
       end
     end
