aedc84b58e10a5f7e9593776463f0bc8d07c6c16
Adam Daniels <adam@mediadrive.ca>
2024-02-27 12:35:35 -0500

    [core] Enable reloading if running in development

--- framework-core/lib/framework/plugins/loader.rb
+++ framework-core/lib/framework/plugins/loader.rb
@@ -2,10 +2,15 @@ module Framework
   module Plugins
     module Loader
       def self.before_load(application, log: false, &block)
+        application.plugin Variant
+
         require "zeitwerk"
 
         loader = Zeitwerk::Loader.new
         loader.log! if log
+
+        loader.enable_reloading if application.variant.development?
+
         loader.push_dir(File.join(Dir.pwd, "lib"))
         block&.call(loader)
         loader.setup
