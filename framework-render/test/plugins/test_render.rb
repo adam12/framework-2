810685df77eba23ad114fce857614ae49cd8d4c2
Adam Daniels <adam@mediadrive.ca>
2024-03-09 11:45:45 -0500

    [render] fix tests

--- framework-render/test/plugins/test_render.rb
+++ framework-render/test/plugins/test_render.rb
@@ -1,12 +1,15 @@
 require "test_init"
 require "framework"
+require "framework-web"
 require "framework-render"
+require "framework/web/resolver"
 
 module Framework
   module Plugins
     class TestRender < Minitest::Test
       def test_render
         application = Class.new(Framework::Application) do
+          plugin Framework::Plugins::Http
           plugin Framework::Plugins::Render
         end
 
@@ -16,11 +19,16 @@ module Framework
           end
         end
 
-        assert_equal "Render test string\n", action.new.call(nil, nil, nil)
+        request_context = Framework::Resolver::RequestContext.build(
+          env: Rack::MockRequest.env_for("/"),
+          application: application.build
+        )
+        assert_equal "Render test string\n", action.new.call(request_context)
       end
 
       def test_render_with_layout
         application = Class.new(Framework::Application) do
+          plugin Framework::Plugins::Http
           plugin Framework::Plugins::Render
         end
 
@@ -30,11 +38,16 @@ module Framework
           end
         end
 
-        assert_equal "Layout with Render test string\n\n", action.new.call(nil, nil, nil)
+        request_context = Framework::Resolver::RequestContext.build(
+          env: Rack::MockRequest.env_for("/"),
+          application: application.build
+        )
+        assert_equal "Layout with Render test string\n\n", action.new.call(request_context)
       end
 
       def test_render_with_locals
         application = Class.new(Framework::Application) do
+          plugin Framework::Plugins::Http
           plugin Framework::Plugins::Render
         end
 
@@ -44,7 +57,11 @@ module Framework
           end
         end
 
-        assert_equal "Render with locals foobar\n", action.new.call(nil, nil, nil)
+        request_context = Framework::Resolver::RequestContext.build(
+          env: Rack::MockRequest.env_for("/"),
+          application: application.build
+        )
+        assert_equal "Render with locals foobar\n", action.new.call(request_context)
       end
     end
   end
