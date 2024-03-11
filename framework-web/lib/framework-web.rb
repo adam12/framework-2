1168bef3e77ca611ed9aca23a1eef91b237fd594
Adam Daniels <adam@mediadrive.ca>
2024-03-11 14:51:12 -0400

    [web] HTTP session plugin

--- framework-web/lib/framework-web.rb
+++ framework-web/lib/framework-web.rb
@@ -3,5 +3,6 @@ module Framework
     autoload :H, __dir__ + "/framework/plugins/h.rb"
     autoload :Http, __dir__ + "/framework/plugins/http.rb"
     autoload :HttpRouter, __dir__ + "/framework/plugins/http_router.rb"
+    autoload :HttpSession, __dir__ + "/framework/plugins/http_session.rb"
   end
 end
---
+++ framework-web/lib/framework/plugins/http_session.rb
@@ -0,0 +1,15 @@
+module Framework
+  module Plugins
+    module HttpSession
+      def self.before_load(application, secret: ENV.fetch("SESSION_SECRET"))
+        application.config.http_router.middleware.use ::Rack::Session::Cookie, secret: secret
+      end
+
+      module ActionMethods
+        def session
+          request.env["rack.session"]
+        end
+      end
+    end
+  end
+end
