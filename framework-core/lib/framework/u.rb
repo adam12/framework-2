1e345c0252d2d3f45499948dad33996db4411b8f
Adam Daniels <adam@mediadrive.ca>
2024-04-01 20:40:33 -0400

    Expand U decorator utility

--- framework-core/lib/framework/u.rb
+++ framework-core/lib/framework/u.rb
@@ -9,13 +9,45 @@ module Framework
       def squeeze_heredoc
         delete("\n").gsub(/\s+/, " ")
       end
+
+      def blank?
+        strip.empty?
+      end
+
+      def present?
+        !blank?
+      end
+    end
+
+    class NilDecorator < DelegateClass(NilClass)
+      def blank?
+        true
+      end
+
+      def present?
+        false
+      end
+    end
+
+    class IntegerDecorator < DelegateClass(Integer)
+      def blank?
+        false
+      end
+
+      def present?
+        true
+      end
     end
 
     # Set up decorator utility for provided value
     def self.[](value)
       case value
+      when nil
+        NilDecorator.new(value)
       when String
         StringDecorator.new(value)
+      when Integer
+        IntegerDecorator.new(value)
       else
         value
       end
