8a78e6cff134ef2c32ea2fa5f1d9a27435708106
Adam Daniels <adam@mediadrive.ca>
2024-04-02 09:12:54 -0400

    [core] Utils.deprecated can accept an uplevel

--- framework-core/lib/framework/utils.rb
+++ framework-core/lib/framework/utils.rb
@@ -7,8 +7,8 @@ module Framework
     autoload :Hash, __dir__ + "/utils/hash"
 
     # Warn functionality has been deprecated with original callsite location
-    def deprecated(msg)
-      warn msg, category: :deprecated, uplevel: 1
+    def deprecated(msg, uplevel: 1)
+      warn msg, category: :deprecated, uplevel: uplevel
     end
     module_function :deprecated
 
