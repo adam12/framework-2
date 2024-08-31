358e160158993725be2bdcaad43eba087968bbff
Adam Daniels <adam@mediadrive.ca>
2024-08-30 21:34:13 -0400

    [web] Move FlashHash class outside of Middleware namespace

--- framework-web/lib/framework/plugins/http_flash.rb
+++ framework-web/lib/framework/plugins/http_flash.rb
@@ -1,6 +1,30 @@
 module Framework
   module Plugins
     module HttpFlash
+      # :nodoc:
+      # FlashHash implementation borrowed from github.com/jeremyevans/roda
+      # :doc
+      class FlashHash < DelegateClass(Hash)
+        attr_reader :next
+
+        def initialize(hash)
+          @next = {}
+          super
+        end
+
+        alias_method :now, :__getobj__
+
+        def []=(key, value)
+          @next[key] = value
+        end
+
+        def sweep
+          replace(@next)
+          @next.clear
+          self
+        end
+      end
+
       module ActionMethods
         def flash
           request.env["_flash"]
@@ -8,30 +32,6 @@ module Framework
       end
 
       class Middleware
-        # :nodoc:
-        # FlashHash implementation borrowed from github.com/jeremyevans/roda
-        # :doc
-        class FlashHash < DelegateClass(Hash)
-          attr_reader :next
-
-          def initialize(hash)
-            @next = {}
-            super
-          end
-
-          alias_method :now, :__getobj__
-
-          def []=(key, value)
-            @next[key] = value
-          end
-
-          def sweep
-            replace(@next)
-            @next.clear
-            self
-          end
-        end
-
         def initialize(app, opts = {})
           @app = app
           @opts = opts
